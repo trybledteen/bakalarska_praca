58b3cdb078b6b704696ac72e240d9d84b6d03e8f
trybledteen <trybledteen@gmail.com>
2026-02-15 13:12:34 +0100

    Product card layout and added Card.vue components

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
-  import Header from './components/Header.vue';
+  import Header from './components/Header.vue'
+  import Card from './components/Card.vue';
 </script>
 
 <template>
-  <div class="bg-gray-200 w-4/5 m-auto h-screen rounded-3xl shadow-xl mt-14">
+  <div class="bg-gray-200 w-4/5 m-auto rounded-3xl shadow-xl mt-14">
     <Header />
-    <h1>Sneakers</h1>
+   
+    <div>
+      <Card />
+    </div>
   </div>
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="relative m-10 border border-gray-300 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
+        <img src="/src/assets/icons/heart.svg" alt="Like 1" class=" w-8 absolute top-8 left-8"/>
+        <img src="/src/assets/sneakers/sneaker1.png" alt="Sneaker"/>
+        
+        <p class="mt-2">ADIDAS CAMPUS 00S</p>
+
+        <div class="flex justify-between mt-5">
+          <div class="flex flex-col">
+            <span class="text-gray-600">Cena:</span>
+            <b>120 €</b>
+          </div>
+
+          <img src="/src/assets/icons/plus.svg" alt="Plus" class="w-7"/>
+        </div>
+      </div>  
+</template>
