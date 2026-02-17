a9feec145d5882d6ab3bd0c2584d9e728b0cbf91
trybledteen <trybledteen@gmail.com>
2026-02-17 19:00:31 +0100

    Fix Header, implement search and filtering for women's and men's sneakers

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@
 </script>
 
 <template>
-  <Drawer /> 
+  <!--<Drawer /> -->
   <div class="bg-gray-200 w-4/5 m-auto rounded-3xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Všetky tenisky</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Všetky tenisky</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none focus: border-gray-300">
+            <option>Dámske</option>
+            <option>Pánske</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3 w-4" src="./assets/icons/search.svg">
+            <input 
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus: border-gray-300" 
+              type="text" 
+              placeholder="Hľadať..."
+            />
+          </div>
+        </div>
+      </div>
 
   
       <CardList />
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <header class="flex justify-between border-b border-gray-300 px-10 py-5">
+    <header class="flex justify-between border-b border-gray-300 px-10 py-4">
 
       <div class="flex items-center gap-4">
         <img src="/src/assets/logo.png" alt="logo" class="w-11"/>
@@ -9,22 +9,17 @@
       </div>
 
       <ul class="flex items-center gap-7">
-        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
-          <img src="/src/assets/icons/search.svg" alt="cart" class="w-6"/>
-          <span class="uppercase">Hľadať</span>
-        </li>
-
-        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+        <li class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-black">
           <img src="/src/assets/icons/shopping-cart.svg" alt="cart" class="w-6"/>
           <b class="uppercase">120 €</b>
         </li>
 
-        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+        <li class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-black">
           <img src="/src/assets/icons/heart.svg" alt="cart" class="w-6"/>
           <span class="uppercase">Schránka</span>
         </li>
 
-        <li class="flex items-center cursor-pointer gap-3 text-gray-600 hover:text-black">
+        <li class="flex items-center cursor-pointer gap-2 text-gray-600 hover:text-black">
           <img src="/src/assets/icons/user.svg" alt="cart" class="w-6"/>
           <span class="uppercase">Profil</span>
         </li>
