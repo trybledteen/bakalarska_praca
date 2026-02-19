7b4a76611bc7af94094525f9ada2673fc3b7533d
trybledteen <trybledteen@gmail.com>
2026-02-19 18:41:20 +0100

    Drawer: connect store, show totalPrice, disable button on empty cart

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,28 +1,31 @@
 <script setup>
-import DrawerHead from './DrawerHead.vue';
-import CartItemList from './CartItemList.vue';
+import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
+import { useSneakersStore } from '../store/useSneakersStore.js'
+
+const store = useSneakersStore()
 </script>
 
 <template>
-    <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
-    <div class="bg-gray-200 w-96 h-full fixed right-0 top-0 z-20 p-8">
-        <DrawerHead />
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-gray-200 w-96 h-full fixed right-0 top-0 z-20 p-8 flex flex-col">
+    <DrawerHead />
 
-        <CartItemList />
+    <CartItemList />
 
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
+        <span>Celkom:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>{{ store.totalPrice }} €</b>
+      </div>
 
-       <div class="flex flex-col gap-4 mt-7">
-            <div class="flex gap-2">
-                <span>Celkom:</span>
-                <div class="flex-1 border-b border-dashed"></div>
-                <b>120 €</b>
-            </div>
-       
-            <button 
-            disabled=""
-            class=" mt-4 transition bg-lime-500 w-full rounded-xl py-2 text-white disabled:bg-gray-500 hover:bg-lime-600 active:bg-lime-700 cursor-pointer">
-            Objednať
-            </button>
-        </div>
+      <button
+        :disabled="store.cart.length === 0"
+        class="mt-4 transition bg-lime-500 w-full rounded-xl py-2 text-white disabled:bg-gray-500 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+      >
+        Objednať
+      </button>
     </div>
+  </div>
 </template>
