67feef2965d2c9da8c0a190dbc1f2855df3da5b4
trybledteen <trybledteen@gmail.com>
2026-02-19 18:39:20 +0100

    CartItemList: render cart from store, add empty state

--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -1,11 +1,23 @@
 <script setup>
-    import CartItem from './CartItem.vue';
+import CartItem from './CartItem.vue'
+import { useSneakersStore } from '../store/useSneakersStore.js'
+
+const store = useSneakersStore()
 </script>
 
 <template>
-    <div class="flex flex-col flex-1 gap-4 justify-between">
-       <CartItem />
-       <CartItem />
-       <CartItem />
-    </div>
+  <div class="flex flex-col flex-1 gap-4">
+    <CartItem
+      v-for="item in store.cart"
+      :key="item.id"
+      :id="item.id"
+      :title="item.title"
+      :imageUrl="item.imageUrl"
+      :price="item.price"
+    />
+
+    <p v-if="store.cart.length === 0" class="text-gray-400 text-center mt-10">
+      Košík je prázdny
+    </p>
+  </div>
 </template>
