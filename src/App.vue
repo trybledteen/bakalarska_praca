ae79242b940df5d07b45c730d7ee676d62e7285f
trybledteen <trybledteen@gmail.com>
2026-02-19 18:51:26 +0100

    add drawer open/close logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
-  import Header from './components/Header.vue'
-  import CardList from './components/CardList.vue'
-  import Drawer from './components/Drawer.vue';
+import { ref } from 'vue'
+import Header from './components/Header.vue'
+import CardList from './components/CardList.vue'
+import Drawer from './components/Drawer.vue'
+
+const isDrawerOpen = ref(false)
 </script>
 
 <template>
-  <Drawer /> 
+  <Drawer v-if="isDrawerOpen" />
+
   <div class="bg-gray-200 w-4/5 m-auto rounded-3xl shadow-xl mt-14">
-    <Header />
+    <Header :onOpenDrawer="() => (isDrawerOpen = !isDrawerOpen)" />
 
     <div class="p-10">
       <div class="flex justify-between items-center">
@@ -15,22 +19,22 @@
 
         <div class="flex gap-4">
           <select class="py-2 px-3 border rounded-md outline-none focus: border-gray-300">
-            <option>Dámske</option>
-            <option>Pánske</option>
+            <option value="">Všetky</option>
+            <option value="dámske">Dámske</option>
+            <option value="pánske">Pánske</option>
           </select>
 
           <div class="relative">
-            <img class="absolute left-4 top-3 w-4" src="./assets/icons/search.svg">
-            <input 
-              class="border rounded-md py-2 pl-11 pr-4 outline-none focus: border-gray-300" 
-              type="text" 
+            <img class="absolute left-4 top-3 w-4" src="./assets/icons/search.svg" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus: border-gray-300"
+              type="text"
               placeholder="Hľadať..."
             />
           </div>
         </div>
       </div>
 
-  
       <CardList />
     </div>
   </div>
