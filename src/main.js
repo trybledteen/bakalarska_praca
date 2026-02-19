c5da177909a3f71ee18f55c6fcbfd801e7e46f72
trybledteen <trybledteen@gmail.com>
2026-02-19 17:37:29 +0100

    install and setup Pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
+const app = createApp(App)
+app.use(createPinia())
 createApp(App).mount('#app')
